c70dadc648b52f1c82df14a4e7f795d819bc0068
Liutong Chen <liutong.clt@gmail.com>
2017-06-14 12:28:06 -0700

    TextInput folder created

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,34 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends React.Component {
+  constructor(props, context) { //what is context used for?
+    super(props, context);
+  }
+
+  render() {
+    return(
+      <h1>Manage course</h1>
+    );
+  }
+}
+
+ManageCoursePage.PropTypes = { //Question: shouldn't this be ".propTypes"?
+  //myProp: PropTypes.string.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    state: state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
